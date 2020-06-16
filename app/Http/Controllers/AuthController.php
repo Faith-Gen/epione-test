964d2f72aff5c69e793a3213ad0778525e368c1c
innoflash <innocentmazando@gmail.com>
2020-06-16 07:27:03 +0200

    nailed login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,16 +7,33 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    /**
+     * Logs in the user.
+     *
+     * @param LoginRequest $request
+     * @return void
+     */
     public function login(LoginRequest $request)
     {
-        if(!$token = auth()->attempt($request->validated())){
+        if (!$token = auth()->attempt($request->validated())) {
             abort(401, 'Invalid login credentials');
         }
 
         return $this->respondWithToken($token);
     }
 
-    private function respondWithToken(string $token){
-
+    /**
+     * Formats successful auth attempts.
+     *
+     * @param string $token
+     * @return void
+     */
+    private function respondWithToken(string $token)
+    {
+        return $this->apiDataResponse([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
     }
 }
