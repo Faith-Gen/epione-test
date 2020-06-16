5e40487f9b8aac660650b3c2dc7a93a7025504d3
innoflash <innocentmazando@gmail.com>
2020-06-16 06:51:23 +0200

    seeding users

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'email' => 'test@email.com'
+        ]);
+        
+
+        factory(User::class, 99)->create();
+    }
+}
